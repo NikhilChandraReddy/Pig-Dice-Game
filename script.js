f9509e31286ef519f46326cc87391e2b16c326cc
Nikhil Reddy <48628715+NikhilChandraReddy@users.noreply.github.com>
2023-09-24 12:44:04 -0400

    Complete game logic

--- script.js
+++ script.js
@@ -12,20 +12,30 @@ const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 
-//Starting conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
+
+function init() {
+  //Starting conditions
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  currentScore0El.textContent = 0;
+  currentScore0El.textContent = 0;
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+}
 
 // Rolling dice funtionality
 const randomNumber = function () {
   return Math.ceil(Math.random() * 6);
 };
-
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
 
 function switchActivePlayer() {
   activePlayer = activePlayer === 0 ? 1 : 0;
@@ -33,6 +43,9 @@ function switchActivePlayer() {
   player0El.classList.toggle("player--active");
   player1El.classList.toggle("player--active");
 }
+
+// set intital conditions
+init();
 
 // Rolling Dice
 btnRoll.addEventListener("click", function () {
@@ -77,12 +90,5 @@ btnHold.addEventListener("click", function () {
 });
 
 btnNew.addEventListener("click", function () {
-  if (playing) {
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    diceEl.classList.add("hidden");
-    currentScore = 0;
-    document.getElementById(`current--0`).textContent = currentScore;
-    document.getElementById(`current--1`).textContent = currentScore;
-  }
+  init();
 });
