206a184d8cead355e8b57f33ba4e6fdfd22d8f90
Nikhil Reddy <48628715+NikhilChandraReddy@users.noreply.github.com>
2023-09-24 12:28:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -26,6 +26,13 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+function switchActivePlayer() {
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+}
+
 // Rolling Dice
 btnRoll.addEventListener("click", function () {
   const diceNumber = randomNumber();
@@ -38,11 +45,36 @@ btnRoll.addEventListener("click", function () {
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
   } else {
-    currentScore = 0;
+    // Swtich to next Player
     document.getElementById(`current--${activePlayer}`).textContent =
       currentScore;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle("player--active");
-    player1El.classList.toggle("player--active");
+    switchActivePlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  scores[activePlayer] += currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 20) {
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--winner");
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add("player--active");
+  } else {
+    // Swtich to next Player
+    switchActivePlayer();
+  }
+});
+
+btnNew.addEventListener("click", function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  diceEl.classList.add("hidden");
+  currentScore = 0;
+  document.getElementById(`current--0`).textContent = currentScore;
+  document.getElementById(`current--1`).textContent = currentScore;
 });
